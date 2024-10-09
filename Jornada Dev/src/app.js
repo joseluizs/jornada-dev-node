ba277bd1b743dc1cf5390a1a255b6a6f90a1d305
joseluizs <joseluiz.smarques@gmail.com>
2024-10-09 15:48:40 -0300

    Usar `Router` e finalizar projeto
    Criação do gitignore

--- Jornada Dev/src/app.js
+++ Jornada Dev/src/app.js
@@ -1,14 +1,13 @@
 import express from 'express'
-import SelecaoController from './app/controllers/SelecaoController.js'
+import routes from './routes.js'
 
 const app = express()
+
+//usar o router
+app.use(routes)
+
+
 app.use(express.json())
 
-//ROTAS
-app.post('/selecoes', SelecaoController.store)
-app.get('/selecoes', SelecaoController.index)
-app.get('/selecoes/:id', SelecaoController.show)
-app.put('/selecoes/:id', SelecaoController.update)
-app.delete('/selecoes/:id', SelecaoController.delete)
 
 export default app
--- Jornada Dev/src/server.js
+++ Jornada Dev/src/server.js
@@ -1,7 +1,6 @@
 import app from "./app.js"
 
-const PORT = 3000
-
+const PORT = process.env.PORT || 3000
 
 //escutar a porta
 app.listen(PORT, () => {
