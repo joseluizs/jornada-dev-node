2d35fb1b132c30fb8a6e39f8e12947e72bafbf7c
joseluizs <joseluiz.smarques@gmail.com>
2024-10-09 08:14:23 -0300

    Criação da `conexao` com a base de dados

--- Jornada Dev/server.js
+++ Jornada Dev/server.js
@@ -1,8 +1,19 @@
 import app from "./src/app.js"
+import conexao from "./infra/conexao.js"
 
 const PORT = 3000
 
-//escutar a porta
-app.listen(PORT, () => {
-    console.log(`Servidor rodando localmente http://localhost:${PORT}`)
+conexao.connect((error) => {
+    if (error) {
+        console.log(error)
+    } else {
+        console.log('Conectado')
+
+        //escutar a porta
+        app.listen(PORT, () => {
+            console.log(`Servidor rodando localmente http://localhost:${PORT}`)
+        })
+    }
 })
+
+
